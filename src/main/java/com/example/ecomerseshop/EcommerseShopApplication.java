ea574d2606a3cb5f2a2cd421a3155450bf31d1c1
kimnamonb@gmail.com <kimnamonb@gmail.com>
2024-03-23 01:07:29 +0600

    add new build tool maven(for him add pom.xml and wrappers), in pom.xml add spring boot starter version 3.2.4 and main class for start application and .gitignore

---
+++ src/main/java/com/example/ecomerseshop/EcommerseShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecomerseshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerseShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerseShopApplication.class, args);
+	}
+
+}

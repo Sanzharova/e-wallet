e4c0450d508da5fc1919beaee5723f3357dcb275
kimnamonb@gmail.com <kimnamonb@gmail.com>
2024-03-23 18:56:25 +0600

    add specification api in UserRepository

--- src/main/java/com/example/ecomerseshop/repository/UserRepository.java
+++ src/main/java/com/example/ecomerseshop/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.example.ecomerseshop.repository;
 
 import com.example.ecomerseshop.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+public interface UserRepository extends JpaRepository<UserEntity, Integer>, JpaSpecificationExecutor<UserEntity> {
 }

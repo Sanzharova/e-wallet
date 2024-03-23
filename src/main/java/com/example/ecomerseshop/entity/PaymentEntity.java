25cab5e18178060b838f996979c91832db087406
kimnamonb@gmail.com <kimnamonb@gmail.com>
2024-03-23 12:32:51 +0600

    add new entity for Payment and his status

---
+++ src/main/java/com/example/ecomerseshop/entity/PaymentEntity.java
@@ -0,0 +1,57 @@
+package com.example.ecomerseshop.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "payments")
+public class PaymentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Integer id;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    PaymentStatus status;
+
+    @Column(name = "created_at")
+    Timestamp created_at;
+
+    @Column(name = "updated_at")
+    Timestamp updated_at;
+
+    @Column(name = "sum_of_favour")
+    BigDecimal sumOfFavour;
+
+    @Column(name = "account_check")
+    String accountCheck;
+
+    @Column(name = "is_checked")
+    Boolean isChecked;
+
+    @Column(name = "is_finished")
+    Boolean finished;
+
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "favour_id")
+    private FavourEntity favour;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "wallet_id")
+    private WalletEntity wallet;
+
+}
---
+++ src/main/java/com/example/ecomerseshop/entity/PaymentStatus.java
@@ -0,0 +1,8 @@
+package com.example.ecomerseshop.entity;
+
+public enum PaymentStatus {
+    STATUS_CREATED,
+    STATUS_PROCESS,
+    STATUS_SUCCESS,
+    STATUS_ROLLBACK
+}

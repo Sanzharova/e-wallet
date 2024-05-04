1da5f33c6a4c7ea997a4b5a3f1efeed48514ff9b
kimnamonb@gmail.com <kimnamonb@gmail.com>
2024-05-05 01:31:31 +0600

    add custom exceptions for service

---
+++ src/main/java/com/example/ecomerseshop/exception/TokenRefreshException.java
@@ -0,0 +1,14 @@
+package com.example.ecomerseshop.exception;
+
+
+public class TokenRefreshException extends RuntimeException {
+
+    private final String token;
+    private final String message;
+
+    public TokenRefreshException(String token, String message) {
+        super(String.format("Couldn't refresh token for [%s]: [%s])", token, message));
+        this.token = token;
+        this.message = message;
+    }
+}
